5b452e4583120bbf65eed7195190569720d4de89
lhcamargo <44513594+lhcamargo@users.noreply.github.com>
2020-05-27 13:54:40 -0300

    Estatistica 1.2
    Agora aprendi a esconder conteudos pra valer

--- scripts.js
+++ scripts.js
@@ -6,14 +6,6 @@ function myFunction(){
     quant = document.getElementById('numeros').value;
     var y = document.getElementById("conteudo2");
     var x = document.getElementById("conteudo");
-    y.style.display = "none";
-
-    if (quant > 0){
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-     
-    }
-}
+    y.style.display = "block";
+    x.style.display = "none";  
 }
