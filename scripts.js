5603533b9b2fd75356936f4aa16848ceb1b5a7c0
lhcamargo <44513594+lhcamargo@users.noreply.github.com>
2020-05-27 14:01:22 -0300

    Estatistica 1.3
    Mostra e esconde conteudo

--- scripts.js
+++ scripts.js
@@ -9,3 +9,10 @@ function myFunction(){
     y.style.display = "block";
     x.style.display = "none";  
 }
+function voltar(){
+
+  var x = document.getElementById('conteudo');
+  var y = document.getElementById('conteudo2');
+  y.style.display = "none";
+  x.style.display = "block";
+}
