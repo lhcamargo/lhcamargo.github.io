bd909e6d48fa29db8b6e41dab234c4dc0b7c3f40
stjodas <61805550+stjodas@users.noreply.github.com>
2020-07-03 14:18:55 -0300

    Minha versão (Saulo)
    Quase tudo do front end hauahua
    -Cachorro
    -Efeitos
    -Audio

--- scripts.js
+++ scripts.js
@@ -1,11 +1,24 @@
 var result;
-var form = document.getElementById("formulario");    
-var  quant;
+var form = document.getElementById("formulario");
+var quant;
 
-function myFunction(){
+function myFunction() {
     quant = document.getElementById('numeros').value;
     var y = document.getElementById("conteudo2");
     var x = document.getElementById("conteudo");
     y.style.display = "block";
-    x.style.display = "none";  
+    x.style.display = "none";
 }
+
+//Efeito gay
+
+let XAngle = 5;
+let YAngle = 0;
+const Z = 50;
+
+const world = document.getElementById("world")
+world.addEventListener("mousemove", function(e) {
+    YAngle = -(0.5 - (e.x / world.clientWidth)) * 5;
+    XAngle = (1 - (e.y / world.clientHeight)) * 5 + 1;
+    world.style.transform = "rotateX(" + -XAngle + "deg) rotateY(" + YAngle + "deg)"
+})
