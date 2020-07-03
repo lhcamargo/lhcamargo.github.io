5d220eb9bec4f08b563062216d0484ef3e2874c1
lhcamargo <44513594+lhcamargo@users.noreply.github.com>
2020-07-03 16:03:32 -0300

    debug e js
    nada

--- scripts.js
+++ scripts.js
@@ -16,3 +16,12 @@ function voltar(){
   y.style.display = "none";
   x.style.display = "block";
 }
+
+let XAngle = 5; 
+let YAngle = 0; 
+const Z = 50;
+  
+const world = document.getElementById("world") world.addEventListener("mousemove", function(e) {     
+  YAngle = -(0.5 - (e.x / world.clientWidth)) * 5;    
+  XAngle = (1 - (e.y / world.clientHeight)) * 5 + 1;   
+  world.style.transform = "rotateX(" + -XAngle + "deg) rotateY(" + YAngle + "deg)" })
